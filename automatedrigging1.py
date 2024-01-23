b8f1fefb96d366497e3ecad6edb415a2682a9410
Colin Cheng <29507684+Torchable@users.noreply.github.com>
2024-01-23 15:25:58 -0500

    Update automatedrigging1.py
    * There was a problem where the rig groups would not load in a new Maya file
    
    *Simplified the code slightly

--- automatedrigging1.py
+++ automatedrigging1.py
@@ -13,23 +13,22 @@ def main():
 #User input for prefix name
     x = cmds.promptDialog(title= 'RIG Group Setup', message='Insert Prefix Name')
     if x == 'Confirm':
-        x = cmds.promptDialog(query = True)
-
-    global RIG
+        x = cmds.promptDialog(query = True, text = True)
     
-#Ensures only one RIG group is created
-    if cmds.objExists(RIG):
-        cmds.delete(RIG)
+#Checks if a rig group already exists and deletes it
+    rigListexist = cmds.ls('*_RIG')
+    if len (rigListexist) > 0:
+        cmds.delete(rigListexist)
         
 #Creation of groups
-    CTRL = cmds.group(em=True, n= x + '_' + 'CTRL_GRP')
-    GEO = cmds.group(em=True, n= x + '_' + 'GEO_GRP')
-    JNT = cmds.group(em=True, n= x + '_' + 'JNT_GRP')
-    notouch = cmds.group(em=True, n= x + '_' + 'notouch_GRP')
-    DEF = cmds.group(em=True, n= x + '_' + 'DEF_GRP')
-    IK = cmds.group(em=True, n= x + '_' + 'IK_GRP')
+    CTRL = cmds.group(em=True, n= x + '_CTRL_GRP')
+    GEO = cmds.group(em=True, n= x + '_GEO_GRP')
+    JNT = cmds.group(em=True, n= x + '_JNT_GRP')
+    notouch = cmds.group(em=True, n= x + '_notouch_GRP')
+    DEF = cmds.group(em=True, n= x + '_DEF_GRP')
+    IK = cmds.group(em=True, n= x + '_IK_GRP')
 
 #Creation of rig group
-    RIG = cmds.group(CTRL, GEO, JNT, notouch, DEF, IK, n= x + '_' + 'RIG')
+    RIG = cmds.group(CTRL, GEO, JNT, notouch, DEF, IK, n= x + '_RIG')
 
 main()
