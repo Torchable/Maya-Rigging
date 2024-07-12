db7f59eb14ecc2c1a1135b9b72a6b3390eb7b5f0
Colin Cheng <29507684+Torchable@users.noreply.github.com>
2024-07-12 14:10:48 -0400

    Update automategroups.py

--- automategroups.py
+++ automategroups.py
@@ -9,18 +9,23 @@
 
 import maya.cmds as cmds
 
-def main():
+def main(groups):
 #User input for prefix name
     x = cmds.promptDialog(title= 'RIG Group Setup', message='Insert Prefix Name')
     if x == 'Confirm':
         x = cmds.promptDialog(query = True, text = True)
+        
+    elif x == 'dismiss':
+        return
     
 #Checks if a rig group already exists and deletes it
     rigListexist = cmds.ls('*_RIG')
     if len (rigListexist) > 0:
         cmds.delete(rigListexist)
+    groups(x)
         
 #Creation of groups
+def groups(x):
     CTRL = cmds.group(em=True, n= x + '_CTRL_GRP')
     GEO = cmds.group(em=True, n= x + '_GEO_GRP')
     JNT = cmds.group(em=True, n= x + '_JNT_GRP')
@@ -31,4 +36,4 @@ def main():
 #Creation of rig group
     RIG = cmds.group(CTRL, GEO, JNT, notouch, DEF, IK, n= x + '_RIG')
 
-main()
+main(groups)
